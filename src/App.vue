1af5c7f29c6879f3d39030ff900fab51203c33a2
wu67 <wk7113@gmail.com>
2022-08-16 00:32:34 +0800

    字体指定

--- src/App.vue
+++ src/App.vue
@@ -105,6 +105,8 @@ const locale = ref(zhCn)
 body {
   margin: 0;
   padding: 0;
+  font-family: 'Helvetica Neue', Helvetica, 'PingFang SC', 'Hiragino Sans GB',
+    'Microsoft YaHei', '\5fae\8f6f\96c5\9ed1', Arial, sans-serif;
 }
 
 #app {
